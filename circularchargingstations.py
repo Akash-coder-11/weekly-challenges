e87c3fe193c951d9895e15bd1667b6bd581215c4
Akash-Coder-11 <25lbcs1108@culkomail.in>
2026-09-04 22:46:25 +0530

    Fix bug in charging station logic

--- circularchargingstations.py
+++ circularchargingstations.py
@@ -3,16 +3,16 @@ class Solution:
     def findOrigin(self, charge: list[int],
                    cost: list[int]) -> int:
         sz = len(charge)
-        overall_net = 0
-        current_run = 0
-        possible_ans = 0
+        overallnet = 0
+        currentrun = 0
+        possibleans = 0
         for pos in range(sz):
             step_diff = charge[pos] - cost[pos]
-            overall_net = overall_net + step_diff
-            current_run = current_run + step_diff
-            if 0 > current_run:
-                possible_ans = pos + 1
-                current_run = 0
-        if overall_net < 0:
+            overallnet = overallnet + step_diff
+            currentrun = currentrun + step_diff
+            if 0 > currentrun:
+                possibleans = pos + 1
+                currentrun = 0
+        if overallnet < 0:
             return -1
-        return possible_ans
+        return possibleans
